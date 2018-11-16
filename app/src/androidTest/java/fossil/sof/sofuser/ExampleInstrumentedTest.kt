ce58d4c79968b8ac1647a9d6826213e3993bc4cb
ninhvanluyen <luyen@mysquar.com>
2018-11-16 10:18:24 +0700

    init_project

---
+++ app/src/androidTest/java/fossil/sof/sofuser/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package fossil.sof.sofuser
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("fossil.sof.sofuser", appContext.packageName)
+    }
+}
